3264278579338a35a9893dae633132f88efaab19
bibin14 <bibin14@gmail.com>
2017-04-23 21:02:20 +0300

    Loading a web based URL

--- app/src/main/java/com/bibzsoftworkz/hybridandroidappsample/MainActivity.java
+++ app/src/main/java/com/bibzsoftworkz/hybridandroidappsample/MainActivity.java
@@ -1,13 +1,34 @@
 package com.bibzsoftworkz.hybridandroidappsample;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
+    WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LoadNewContent();
+    }
+
+    @SuppressLint({ "SetJavaScriptEnabled", "JavascriptInterface" })
+    public void LoadNewContent()
+    {
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+
+        //load index.html from the assets folder
+        //mWebView.loadUrl("file:///android_asset/www/index.htm");
+
+        //load a web based URL
+        mWebView.loadUrl("https://github.com/bibin14/HybridAndroidAppSample");
+
+
     }
 }
